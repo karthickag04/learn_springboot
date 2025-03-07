c4d34869a8f25a4b9bd10875a63f8ca7f44caad4
karthickag04 <karthickag04@gmail.com>
2025-03-07 15:13:20 +0530

    enabled Navbar through base.jsp to common to all other pages

--- springfirstapp/src/main/java/com/tmys/springfirstapp/controller/HomeController.java
+++ springfirstapp/src/main/java/com/tmys/springfirstapp/controller/HomeController.java
@@ -1,29 +1,30 @@
 package com.tmys.springfirstapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	
-	@GetMapping({"/","index"})
-	public String HomePage() {
-		return "index";
-		
-	}
-	
-	
 
-	@GetMapping("/service")
-	public String ServicePage() {
-		return "services";
-		
-	}
-	
-	@GetMapping("/about")
-	public String AboutusPage() {
-		return "aboutus";
-		
-	}
+    @GetMapping("/")
+    public String home() {
+        return "index"; // Renders index.jsp
+    }
 
+    @GetMapping("/about")
+    public String about() {
+        return "about"; // Renders about.jsp
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact"; // Renders contact.jsp
+    }
+
+    @GetMapping("/students")
+    public String listStudents(Model model) {
+        // Add logic to fetch students
+        return "students"; // Renders students.jsp
+    }
 }
---
+++ springfirstapp/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,8 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ include file="base.jsp" %>
+
+<c:set var="title" value="About Page" />
+<c:set var="content" value="about.jsp" />
+
+<h2>Index Page</h2>
+<p>This is the index page of our Spring Boot application.</p>
