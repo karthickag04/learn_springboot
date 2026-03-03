6c605a847dc421023d27da253c1fd721b44e7cea
karthickag04 <karthickag04@gmail.com>
2026-03-03 12:08:13 +0530

    added food erp rest controller demo

---
+++ foodapperp/src/test/java/com/trymeyourself/foodapperp/FoodapperpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trymeyourself.foodapperp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodapperpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
